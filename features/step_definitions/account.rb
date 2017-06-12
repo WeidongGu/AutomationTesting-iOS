627447fe9612a0c9a4c3d52287dd8eea5db3cbca
JamesGu <gwd@jamesgu.cc>
2017-06-13 00:52:46 +0800

    引用并调用

--- features/step_definitions/account.rb
+++ features/step_definitions/account.rb
@@ -1,29 +1,20 @@
 当(/^我进入"([^"]*)"页面$/) do |text|
-    assert(driver.get_source.include?(text))
+    GeneralPage.should_see text
 end
 
 当(/^我输入邮箱"([^"]*)"和密码"([^"]*)"$/) do |email , password|
-    element = find_element accessibility_id: "emailTextFiled"
-    element.clear
-    element.send_keys email
-
-    element = find_element accessibility_id: "passwordTextfield"
-    element.clear
-    element.send_keys password
+    GeneralPage.fill_element("邮箱",email)
+    GeneralPage.fill_element("密码",password)
 end
 
 当(/^我点击"([^"]*)"$/) do |element_id|
-    if element_id == "登录"
-        element_id = "loginbutton"
-    end
-    element = find_element accessibility_id: element_id
-    element.click
+    GeneralPage.click_element element_id
 end
 
 当(/^等待"([^"]*)"秒$/) do |text|
-    sleep text.to_i
+    GeneralPage.wait_second text
 end
 
 当(/^我应该看到"([^"]*)"$/) do |text|
-    assert(driver.get_source.include?(text))
+    GeneralPage.should_see text
 end
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,3 +1,9 @@
 require 'appium_lib'
 require 'cucumber/ast'
 require 'test/unit'
+
+#require rb files
+require File.dirname(__FILE__)+'/../pages/general_page.rb'
+
+#include
+include GeneralPage
