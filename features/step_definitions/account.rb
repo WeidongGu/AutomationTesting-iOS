669ab386d495552eac644a543dd95ee4cd4c91df
JamesGu <gwd@jamesgu.cc>
2017-06-13 00:44:04 +0800

    实现邮箱和密码的填充

--- features/step_definitions/account.rb
+++ features/step_definitions/account.rb
@@ -3,6 +3,13 @@
 end
 
 当(/^我输入邮箱"([^"]*)"和密码"([^"]*)"$/) do |email , password|
+    element = find_element accessibility_id: "emailTextFiled"
+    element.clear
+    element.send_keys email
+
+    element = find_element accessibility_id: "passwordTextfield"
+    element.clear
+    element.send_keys password
 end
 
 当(/^我点击"([^"]*)"$/) do |element_id|
